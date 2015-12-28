ab1c2d93f7415be40518557068179502e4e86af4
Aditya Prakash <aditya.prakash132@gmail.com>
2015-12-28 14:52:06 +0530

    added grunt modules

---
+++ Gruntfile.js
@@ -0,0 +1,125 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+  require('time-grunt')(grunt);
+
+  require('jit-grunt')(grunt, {
+    useminPrepare: 'grunt-usemin'
+  });
+  // Define the configuration for all the tasks
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
+      },
+      all: {
+        src: [
+          'Gruntfile.js',
+          'app/scripts/{,*/}*.js'
+        ]
+      }
+     },
+     useminPrepare: {
+        html: 'app/menu.html',
+        options: {
+            dest: 'dist'
+        }
+    },
+    // Concat
+    concat: {
+        options: {
+            separator: ';'
+        },
+        // dist configuration is provided by useminPrepare
+        dist: {}
+    },
+      // Uglify
+    uglify: {
+        // dist configuration is provided by useminPrepare
+        dist: {}
+    },
+    cssmin: {
+        dist: {}
+    },
+      // Filerev
+    filerev: {
+        options: {
+            encoding: 'utf8',
+            algorithm: 'md5',
+            length: 20
+        },
+        release: {
+            // filerev:release hashes(md5) all assets (images, js and css )
+            // in dist directory
+            files: [{
+                src: [
+                    'dist/scripts/*.js',
+                    'dist/styles/*.css',
+                ]
+            }]
+        }
+    },
+    // Usemin
+    // Replaces all assets with their revved version in html and css files.
+    // options.assetDirs contains the directories for finding the assets
+    // according to their relative paths
+    usemin: {
+        html: ['dist/*.html'],
+        css: ['dist/styles/*.css'],
+        options: {
+            assetsDirs: ['dist', 'dist/styles']
+        }
+    },
+    copy: {
+      dist: {
+        cwd: 'app',
+        src: [ '**','!styles/**/*.css','!scripts/**/*.js' ],
+        dest: 'dist',
+        expand: true
+      },
+      fonts: {
+          files:[
+              {
+                //for bootstrap fonts
+                expand: true,
+                dot: true,
+                cwd: 'bower_components/bootstrap/dist',
+                src: ['fonts/*.*'],
+                dest: 'dist'
+              },
+              {
+                //for font-awesome
+                expand: true,
+                dot: true,
+                cwd: 'bower_components/font-awesome',
+                src: ['fonts/*.*'],
+                dest: 'dist'
+              }
+          ]
+        }
+    },
+    clean: {
+      build:{
+          src: [ 'dist/']
+      }
+    }
+  });
+
+  grunt.registerTask('build', ['clean',
+    'jshint',
+    'useminPrepare',
+    'concat',
+    'cssmin',
+    'uglify',
+    'copy',
+    'filerev',
+    'usemin'
+  ]);
+
+  grunt.registerTask('default', ['build']);
+
+};
